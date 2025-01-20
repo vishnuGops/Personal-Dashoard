22ed3541bf8968cba29741fe5d0fe9bf8e87643b
Vishnu Gopal Rajan <35251589+vishnuGops@users.noreply.github.com>
2025-01-20 11:10:20 -0800

    landing component bg video with scrollbar

--- dashboard-client/src/app/components/landing-hero/landing-hero.component.ts
+++ dashboard-client/src/app/components/landing-hero/landing-hero.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-landing-hero',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, MatButtonModule, MatCardModule],
   templateUrl: './landing-hero.component.html',
-  styleUrl: './landing-hero.component.scss'
+  styleUrls: ['./landing-hero.component.scss'],
 })
-export class LandingHeroComponent {
-
-}
+export class LandingHeroComponent {}
