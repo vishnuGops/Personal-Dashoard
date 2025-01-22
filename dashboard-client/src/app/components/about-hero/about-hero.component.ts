ddde515216122946481fde4306f869aad49be3ad
Vishnu Gopal Rajan <35251589+vishnuGops@users.noreply.github.com>
2025-01-21 20:29:24 -0800

    About me landing component added

---
+++ dashboard-client/src/app/components/about-hero/about-hero.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'app-about-hero',
+  standalone: true,
+  imports: [RouterModule, MatButtonModule, MatCardModule],
+  templateUrl: './about-hero.component.html',
+  styleUrl: './about-hero.component.scss'
+})
+export class AboutHeroComponent {
+
+}
--- dashboard-client/src/app/pages/about/about.component.ts
+++ dashboard-client/src/app/pages/about/about.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { AboutHeroComponent } from '../../components/about-hero/about-hero.component';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  standalone: true,
+  imports: [MatToolbarModule, MatCardModule, MatButtonModule, AboutHeroComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
