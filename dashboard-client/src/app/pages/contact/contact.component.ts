8b43b13f410d54bbc56a60bbbf3685939e7bcd53
Vishnu Gopal Rajan <35251589+vishnuGops@users.noreply.github.com>
2025-01-21 20:42:17 -0800

    contact hero component added

--- dashboard-client/src/app/pages/contact/contact.component.ts
+++ dashboard-client/src/app/pages/contact/contact.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { ContactHeroComponent } from '../../components/contact-hero/contact-hero.component';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  standalone: true,
+  imports: [MatToolbarModule, MatCardModule, MatButtonModule, ContactHeroComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
