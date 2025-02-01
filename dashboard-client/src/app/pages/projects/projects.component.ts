121f0f062fc8ac43169498b64cdaf43536a954b6
Vishnu <vishvargops@gmail.com>
2025-02-01 00:59:12 -0800

    Projects Hero

--- dashboard-client/src/app/pages/projects/projects.component.ts
+++ dashboard-client/src/app/pages/projects/projects.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ProjectsHeroComponent } from '../../components/projects-hero/projects-hero.component';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [ProjectsHeroComponent],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
