fb977802ed7422e16c7a070d4cb031e7f749f459
Vishnu <vishvargops@gmail.com>
2025-02-02 22:37:20 -0800

    backend added

---
+++ src/main/java/com/Vishnu/PersonalDashboard/PersonalDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.Vishnu.PersonalDashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Vishnu/PersonalDashboard/PersonalDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Vishnu.PersonalDashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
