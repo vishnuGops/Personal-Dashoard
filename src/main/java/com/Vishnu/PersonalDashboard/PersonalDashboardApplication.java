b53703b6831292cabca7382a748275c266b0331d
Vishnu Gopal Rajan <35251589+vishnuGops@users.noreply.github.com>
2025-02-05 09:03:54 -0800

    persist database and add logging

--- src/main/java/com/Vishnu/PersonalDashboard/PersonalDashboardApplication.java
+++ src/main/java/com/Vishnu/PersonalDashboard/PersonalDashboardApplication.java
@@ -2,12 +2,15 @@ package com.Vishnu.PersonalDashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.logging.Logger;
 
 @SpringBootApplication
 public class PersonalDashboardApplication {
+	private static final Logger logger = Logger.getLogger(PersonalDashboardApplication.class.getName());
 
 	public static void main(String[] args) {
 		SpringApplication.run(PersonalDashboardApplication.class, args);
+		logger.info("PersonalDashboardApplication Started at http://localhost:8080");
 	}
 
 }
--- src/main/java/com/Vishnu/PersonalDashboard/service/StockPortfolioService.java
+++ src/main/java/com/Vishnu/PersonalDashboard/service/StockPortfolioService.java
@@ -1,8 +1,10 @@
 package com.Vishnu.PersonalDashboard.service;
 
+import com.Vishnu.PersonalDashboard.PersonalDashboardApplication;
 import com.Vishnu.PersonalDashboard.model.StockPortfolio;
 import com.Vishnu.PersonalDashboard.repository.StockPortfolioRepository;
 import org.springframework.stereotype.Service;
+import java.util.logging.Logger;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
@@ -12,6 +14,7 @@ import java.util.Optional;
 @Service
 public class StockPortfolioService {
     private final StockPortfolioRepository stockPortfolioRepository;
+    private static final Logger logger = Logger.getLogger(PersonalDashboardApplication.class.getName());
 
     public StockPortfolioService(StockPortfolioRepository stockPortfolioRepository) {
         this.stockPortfolioRepository = stockPortfolioRepository;
@@ -26,6 +29,7 @@ public class StockPortfolioService {
     }
 
     public StockPortfolio saveStock(StockPortfolio stock) {
+        logger.info("Saving stock data into the DB: " + stock);
         stock.setTotalCost(stock.getAvgPrice().multiply(BigDecimal.valueOf(stock.getShares())));
         stock.setTotalValue(stock.getMarketPrice().multiply(BigDecimal.valueOf(stock.getShares())));
         stock.setTotalPL(stock.getTotalValue().subtract(stock.getTotalCost()));
