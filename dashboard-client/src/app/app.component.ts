4255c310b446f26149d44f18fd659b88733bc495
Vishnu Gopal Rajan <35251589+vishnuGops@users.noreply.github.com>
2025-03-07 22:18:52 -0800

    scroll to top of each page

--- dashboard-client/src/app/app.component.ts
+++ dashboard-client/src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +10,22 @@ import { RouterModule } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'dashboard-client';
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+      }
+    });
+  }
+
+  ngAfterViewInit() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
 }
