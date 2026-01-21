3f18c47c3455618d0aed4570fe830f330a989036
Vishnu <35251589+vishnuGops@users.noreply.github.com>
2026-01-21 09:08:21 -0800

    Revert "Fix scroll to top bug + photo wall"
    This reverts commit d3229f3c5a2431e5b555240474210006db2bdc37.

--- vishlabs-client/src/components/Navbar.tsx
+++ vishlabs-client/src/components/Navbar.tsx
@@ -1,41 +1,20 @@
-"use client";
-
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
 import styles from './Navbar.module.scss';
 
 export default function Navbar() {
-  const pathname = usePathname();
-
-  const handleScroll = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>, href: string) => {
-    // If the user is already on the page they are clicking
-    if (pathname === href) {
-      e.preventDefault();
-      // Scroll both body and documentElement to ensure it works across different scroll setups
-      document.body.scrollTo({
-        top: 0,
-        behavior: 'smooth',
-      });
-      document.documentElement.scrollTo({
-        top: 0,
-        behavior: 'smooth',
-      });
-    }
-  };
-
   return (
     <div className={styles['custom-toolbar']}>
       <div className={styles['navbar-logo']}>
-        <Link href="/" onClick={(e) => handleScroll(e, '/')}>
+        <Link href="/">
           <img src="/images/VG_logo.png" alt="Logo" className={styles.logo} />
         </Link>
       </div>
       <span className={styles.spacer}></span>
       <nav>
-        <Link href="/" onClick={(e) => handleScroll(e, '/')}>Home</Link>
-        <Link href="/about" onClick={(e) => handleScroll(e, '/about')}>About</Link>
-        <Link href="/projects" onClick={(e) => handleScroll(e, '/projects')}>Projects</Link>
-        <Link href="/contact" onClick={(e) => handleScroll(e, '/contact')}>Contact</Link>
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+        <Link href="/projects">Projects</Link>
+        <Link href="/contact">Contact</Link>
       </nav>
     </div>
   );
