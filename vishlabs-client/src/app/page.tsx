eff65f38f91baca88d7c74642c38255fb4423f75
Vishnu <35251589+vishnuGops@users.noreply.github.com>
2026-01-23 23:59:09 -0800

    beams react bits landing page bg

--- vishlabs-client/src/app/page.tsx
+++ vishlabs-client/src/app/page.tsx
@@ -2,14 +2,38 @@ import LandingHero from "@/components/LandingHero";
 import LandingInfo from "@/components/LandingInfo";
 import LandingPhotoWall from "@/components/LandingPhotoWall";
 import CareerTimeline from "@/components/CareerTimeline";
+import Beams from "@/components/Beams";
 
 export default function Home() {
   return (
-    <div>
-      <LandingHero />
-      <LandingInfo />
-      <LandingPhotoWall />
-      <CareerTimeline />
+    <div style={{ position: "relative", minHeight: "100vh" }}>
+      <div
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          zIndex: -1,
+        }}
+      >
+        <Beams
+          beamWidth={3}
+          beamHeight={30}
+          beamNumber={20}
+          lightColor="#ffffff"
+          speed={2}
+          noiseIntensity={1.75}
+          scale={0.2}
+          rotation={30}
+        />
+      </div>
+      <div style={{ position: "relative", zIndex: 1 }}>
+        <LandingHero />
+        <LandingInfo />
+        <LandingPhotoWall />
+        <CareerTimeline />
+      </div>
     </div>
   );
 }
