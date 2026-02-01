755e9503a020a36cbebdec77f88a4e16d29bc7bf
Vishnu <35251589+vishnuGops@users.noreply.github.com>
2026-01-31 17:07:43 -0800

    style: adjust width properties and add peer flags in various components

--- vishlabs-client/src/app/page.tsx
+++ vishlabs-client/src/app/page.tsx
@@ -12,7 +12,6 @@ export default function Home() {
           position: "fixed",
           top: 0,
           left: 0,
-          width: "100%",
           height: "100%",
           zIndex: -1,
         }}
