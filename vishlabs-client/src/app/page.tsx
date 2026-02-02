0e8f6ab73e5c6a7316b20f5e0fdea6e45df31f22
Vishnu <vishvargops@gmail.com>
2026-02-01 21:28:27 -0800

    Revert "Merge branch '3.x' of https://github.com/vishnuGops/Personal-Dashoard into 3.x"
    This reverts commit 151e7d07a85fbac1de20930d75cef3d194b69be6, reversing
    changes made to 5c98ed61bf2baab7abcb73a2e060084cc5babecd.

--- vishlabs-client/src/app/page.tsx
+++ vishlabs-client/src/app/page.tsx
@@ -12,13 +12,13 @@ export default function Home() {
           position: "fixed",
           top: 0,
           left: 0,
-          height: "100%",
           width: "100%",
+          height: "100%",
           zIndex: -1,
         }}
       >
         <Beams
-          beamWidth={2}
+          beamWidth={3}
           beamHeight={30}
           beamNumber={20}
           lightColor="#ffffff"
