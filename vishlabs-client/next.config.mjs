918992242d8724de7b1b2d5a6050aa969f02b926
Vishnu <vishvargops@gmail.com>
2026-02-02 23:43:50 -0800

    Refactor component styles: remove width properties from various hero components and navbar for improved responsiveness

--- vishlabs-client/next.config.mjs
+++ vishlabs-client/next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
+        hostname: '**.googleusercontent.com',
       },
       {
         protocol: 'https',
